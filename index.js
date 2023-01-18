9fc2e98f03945174dbf6b2572106c41fa9391bbd
Rytis <ezezke@gmail.com>
2023-01-18 20:23:26 +0200

    added existing user html

--- index.js
+++ index.js
@@ -1,60 +1,81 @@
 const state = {};
 //https://robust-safe-crafter.glitch.me/
 //https://localhost:7205/House/
-function fetchPerson(param){
+function fetchParameter(param) {
   fetch(`https://localhost:7172/${param}`)
-  .then((res) => res.json())
-  .then((data) => {
-    renderFilterUsers([...new Set(data.map((entry) => entry.city))]);
-    renderUserCards(data);
-    state.properties = data;
+    .then((res) => res.json())
+    .then((param) => {
+      renderFilterPerson([...new Set(param.map((name) => name.person))]);
+      renderPersonCards(param);
+      state.person = param;
+    });
+}
+
+fetchParameter("Person");
+fetchParameter("Person/id");
+
+function renderFilterPerson(names) {
+  names.forEach((name) => {
+    const filterButton = document.createElement("button");
+    filterButton.innerText = name;
+    const filterContainer = document.getElementById("filter-container");
+    filterContainer.append(filterButton);
   });
 }
 
-fetchPerson('Person');
-fetchPerson('Person/id');
-
-function renderUserCards(properties) {
-  const userContainer = document.getElementById("properties-container");
-  userContainer.innerHTML = "";
-  properties.forEach((property) => {
-    const userCard = document.createElement("div");
-    userCard.setAttribute("class", "user-card");
-    const userImage = document.createElement("img");
-    userImage.src = property.image;
-    const userName = document.createElement("h1");
-    userName.textContent = user.name;
-    const userSurname = document.createElement("p");
-    userSurname.textContent = user.surname;
+function renderPersonCards(person) {
+  const personContainer = document.getElementById("users-container");
+  personContainer.innerHTML = "";
+  person.forEach((person) => {
+    const personCard = document.createElement("div");
+    personCard.setAttribute("class", "user-card");
+    const personImage = document.createElement("img");
+    personImage.src = person.image;
+    const personName = document.createElement("h1");
+    personName.textContent = person.name;
+    const personSurname = document.createElement("p");
+    personSurname.textContent = person.surname;
     const personsCode = document.createElement("p");
-    personsCode.textContent = user.personsCode;
+    personsCode.textContent = person.personsCode;
 
-    propertyCard.append(
-      userImage,
-      userName,
-      userSurname,
-      personsCode
-    );
-    userContainer.append(userCard);
+    personCard.append(personImage, personName, personSurname, personsCode);
+    personContainer.append(personCard);
   });
+}
+
+function filterPerson(event) {
+  if (event.target.tagName.toLowerCase() === "button") {
+    const isAlreadySelected = event.target.classList.contains("selected");
+    if (isAlreadySelected) {
+      renderPersonCards(state.person);
+    } else {
+      const personNameClicked = event.target.innerText;
+      renderPersonCards(
+        state.person.filter((person) => person.name === personNameClicked)
+      );
+      const allFilterButtons = event.target.parentNode.children;
+      for (let i = 0; i < allFilterButtons.length; i++) {
+        allFilterButtons[i].classList.remove("selected");
+      }
+    }
+    event.target.classList.toggle("selected");
+  }
 }
 
 let base64String = "";
-  function imageUploaded() {
-    var file = document.querySelector("input[type=file]")["files"][0];
-    var reader = new FileReader();
-    console.log("next");
-    reader.onload = function () {
-      base64String = reader.result.replace("data:", "").replace(/^.+,/, "");
-      imageBase64Stringsep = base64String;
-      // alert(imageBase64Stringsep);
-      console.log(base64String);
-    };
-    reader.readAsDataURL(file);
-  }
-  function displayString() {
-    console.log("Base64String about to be printed");
-    alert(base64String);
-  }
-document
-  .getElementById("filter-container")
+function imageUploaded() {
+  var file = document.querySelector("input[type=file]")["files"][0];
+  var reader = new FileReader();
+  console.log("next");
+  reader.onload = function () {
+    base64String = reader.result.replace("data:", "").replace(/^.+,/, "");
+    imageBase64Stringsep = base64String;
+    // alert(imageBase64Stringsep);
+    console.log(base64String);
+  };
+  reader.readAsDataURL(file);
+}
+function displayString() {
+  console.log("Base64String about to be printed");
+  alert(base64String);
+}
